d2de6d9430b2b69d9bafec88140fa30d980d0287
Stuart McCamley <s.mccamley@gmail.com>
2016-05-15 10:39:41 +0100

    added two methods, email and tld

---
+++ README.md
@@ -0,0 +1,14 @@
+rexValidate
+==============
+
+This is a validation library for angular with a simple factory for validating code
+--------------
+
+To use, add 'rexValidator' as a dependency for your app. Then use the factory 'rexValidate' In your app.
+
+These are the methods
+
+- email, for validating email
+- tld, for validating top level domains
+
+    Have fun
---
+++ rexValidate.js
@@ -0,0 +1,21 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('rexValidator',[])
+		.factory('rexValidate', function(){
+			var val = {};
+
+			val.email = function(email){
+				var reg = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/);
+				return reg.test(email);
+			};
+			
+			val.tld = function(domain){
+				var reg = new RegExp(/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/);
+				return reg.test(domain);
+			};
+
+			return val;
+		});
+})();
